aeac0893af17dd6002fbfaeee9d5f7dc86f644fd
ReynaGarL <reyna.g.gar.l@gmail.com>
2025-06-28 17:18:20 -0600

    Primera version, esqueleto aplicacion

---
+++ ProyectoFinal/src/app.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const hbs = require('hbs')
+const express = require('express')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`)
+})
+
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Reyna Garcia'
+    })
+})
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
